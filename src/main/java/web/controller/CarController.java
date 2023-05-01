1de5e701ba3967e1d91869d20599994afa54c670
HautZ532 <113337899+HautZ532@users.noreply.github.com>
2023-05-01 19:20:42 +0300

    Corrected version

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -11,16 +11,15 @@ import java.util.List;
 
 @Controller
 public class CarController {
+    private final CarServiceImp carService;
+
+    public CarController(CarServiceImp carService) {
+        this.carService = carService;
+    }
 
 
     @GetMapping(value = "/cars")
     public String printCar(@RequestParam(value = "count", defaultValue = "5") int count, ModelMap model) {
-        CarServiceImp carService = new CarServiceImp();
-        carService.addCarInList(new Car("Audi", "A5", "NHT678KL"));
-        carService.addCarInList(new Car("Mazda", "6", "KYU983QV"));
-        carService.addCarInList(new Car("Nissan", "Almera", "PIT459sSD"));
-        carService.addCarInList(new Car("Toyota", "Camri", "REY943TH"));
-        carService.addCarInList(new Car("Lada", "Vesta", "NHT678KL"));
         List<Car> allCars = carService.getCarFromListByCount(count);
         model.addAttribute("messages", allCars);
         return "cars";
--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -1,12 +1,21 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.dao.CarDaoImp;
 import web.model.Car;
 import java.util.List;
 
+@Service
 public class CarServiceImp implements CarService {
     private final CarDaoImp car = new CarDaoImp();
 
+    {
+        car.addCarInList(new Car("Audi", "A5", "NHT678KL"));
+        car.addCarInList(new Car("Mazda", "6", "KYU983QV"));
+        car.addCarInList(new Car("Nissan", "Almera", "PIT459sSD"));
+        car.addCarInList(new Car("Toyota", "Camri", "REY943TH"));
+        car.addCarInList(new Car("Lada", "Vesta", "NHT678KL"));
+    }
 
     @Override
     public void addCarInList(Car car) {
